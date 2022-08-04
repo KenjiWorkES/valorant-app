15271f742b3b2c7976585088c86307b2d6b73332
KenjiLucas <802.182@alunos.unigran.br>
2022-08-04 16:23:59 -0400

    add media query to header and searchInput (desktop and tablet)

--- components/organisms/Header/index.jsx
+++ components/organisms/Header/index.jsx
@@ -4,18 +4,20 @@ import { SearchInput } from "../../atoms";
 const Header = () => {
   return (
     <header className="header">
-      <img
-        className="header__logo"
-        src="/logo.svg"
-        alt="Valorant website logo"
-      />
-      <nav className="header__nav">
-        <HeaderList />
-        <SearchInput />
-      </nav>
-      <button className="header__burguer">
-        <span className="header__line"></span>
-      </button>
+      <div className="header__container">
+        <img
+          className="header__logo"
+          src="/logo.svg"
+          alt="Valorant website logo"
+        />
+        <nav className="header__nav">
+          <HeaderList />
+          <SearchInput />
+        </nav>
+        <button className="header__burguer">
+          <span className="header__line"></span>
+        </button>
+      </div>
     </header>
   );
 };
