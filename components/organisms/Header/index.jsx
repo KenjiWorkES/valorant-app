8580a9a6f7048ed889322e8c19405308a7639a46
KenjiLucas <802.182@alunos.unigran.br>
2022-08-04 16:44:03 -0400

    add function to the menu

--- components/organisms/Header/index.jsx
+++ components/organisms/Header/index.jsx
@@ -1,7 +1,19 @@
 import { HeaderList } from "../../molecules";
 import { SearchInput } from "../../atoms";
+import { useState } from "react";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const navClasses = `header__nav ${isActive ? "header__nav--active" : ""}`;
+  const lineClasses = `header__line ${isActive ? "header__line--active" : ""}`;
+
+  const activeMenuHandler = () => {
+    setIsActive((prev) => {
+      return !prev;
+    });
+  };
+
   return (
     <header className="header">
       <div className="header__container">
@@ -10,12 +22,12 @@ const Header = () => {
           src="/logo.svg"
           alt="Valorant website logo"
         />
-        <nav className="header__nav">
+        <nav className={navClasses}>
           <HeaderList />
           <SearchInput />
         </nav>
-        <button className="header__burguer">
-          <span className="header__line"></span>
+        <button onClick={activeMenuHandler} className="header__burguer">
+          <span className={lineClasses}></span>
         </button>
       </div>
     </header>
