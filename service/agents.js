b1e8f3580a8c3c4e717b0c8ce144135e5d61bc60
KenjiLucas <802.182@alunos.unigran.br>
2022-08-08 16:24:26 -0400

    take off duplica sova in api with isPlayableCaracter

--- service/agents.js
+++ service/agents.js
@@ -1,4 +1,5 @@
-const url = "https://valorant-api.com/v1/agents";
+const url =
+  "https://valorant-api.com/v1/agents?language=pt-BR&isPlayableCharacter=true";
 
 export const getAllAgents = async () => {
   const response = await fetch(url);
