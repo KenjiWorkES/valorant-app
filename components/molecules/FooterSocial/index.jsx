f4d19e8c22685be674566f11c6f425ba25b7013d
KenjiLucas <802.182@alunos.unigran.br>
2022-08-08 13:10:22 -0400

    add mobile style to footer

--- components/molecules/FooterSocial/index.jsx
+++ components/molecules/FooterSocial/index.jsx
@@ -3,7 +3,9 @@ import { FooterIcons } from "../../atoms";
 const FooterSocial = () => {
   return (
     <div className="footer__social">
-      <p>Desenvolvimento por Lucas Kenji e Design por Pedro Sander</p>
+      <p className="footer__socialText">
+        Desenvolvimento por Lucas Kenji e Design por Pedro Sander
+      </p>
       <FooterIcons />
     </div>
   );
