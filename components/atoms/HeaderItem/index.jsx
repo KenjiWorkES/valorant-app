5cd08c33b198da4f2e032d445274e714f39d6e76
KenjiLucas <802.182@alunos.unigran.br>
2022-08-09 15:48:10 -0400

    add link to all header

--- components/atoms/HeaderItem/index.jsx
+++ components/atoms/HeaderItem/index.jsx
@@ -1,5 +1,13 @@
-const HeaderItem = ({ text }) => {
-  return <li className="header__item">{text}</li>;
+import Link from "next/link";
+
+const HeaderItem = ({ text, link }) => {
+  return (
+    <Link href={link}>
+      <a className="header__link">
+        <li className="header__item">{text}</li>
+      </a>
+    </Link>
+  );
 };
 
 export default HeaderItem;
--- components/molecules/HeaderList/index.jsx
+++ components/molecules/HeaderList/index.jsx
@@ -3,11 +3,11 @@ import { HeaderItem } from "../../atoms";
 const HeaderList = () => {
   return (
     <ul className="header__list">
-      <HeaderItem text="Home" />
-      <HeaderItem text="Agentes" />
-      <HeaderItem text="Mapas" />
-      <HeaderItem text="Modos de Jogos" />
-      <HeaderItem text="Ranques" />
+      <HeaderItem text="Home" link="/" />
+      <HeaderItem text="Agentes" link="/agentes" />
+      <HeaderItem text="Mapas" link="/mapas" />
+      <HeaderItem text="Modos de Jogos" link="/modos-de-jogo" />
+      <HeaderItem text="Ranques" link="/ranques" />
     </ul>
   );
 };
