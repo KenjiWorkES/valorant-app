b028f92e5d445166d90f06d67f39c42cc7dc4947
KenjiLucas <802.182@alunos.unigran.br>
2022-08-11 12:39:39 -0400

    add style to the mapsPage (mobile and desktop)

--- components/atoms/MapItem/index.jsx
+++ components/atoms/MapItem/index.jsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const MapItem = ({ uuid, name, image }) => {
   return (
     <Link href={`/mapas/${uuid}`}>
       <a className="mapsPage__link">
         <li className="mapsPage__item">
-          <img className="mapsPage__image" src={image} alt={`Mapa ${name}`} />
+          <Image
+            width={626}
+            height={352}
+            className="mapsPage__image"
+            src={image}
+            alt={`Mapa ${name}`}
+          />
           <h3 className="mapsPage__name">{name}</h3>
         </li>
       </a>
