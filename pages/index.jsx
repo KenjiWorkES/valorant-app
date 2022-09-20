901ebd130675b821e531b0c6b0dcdb8b510a2ad9
KenjiWorkES <lucas.kenjies@gmail.com>
2022-09-19 22:16:26 -0300

    add favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,25 +4,30 @@ import {
   GameModes,
   HomeMaps,
   HomeRanks,
-} from "../components/organisms";
+} from '../components/organisms';
 
-import { useRouter } from "next/router";
+import Head from 'next/head';
 
-import { getAllAgents } from "../service/agents";
-import { getAllGameModes } from "../service/modes";
-import { getAllMaps } from "../service/maps";
-import { getAllRanks } from "../service/ranks";
+import { useRouter } from 'next/router';
+
+import { getAllAgents } from '../service/agents';
+import { getAllGameModes } from '../service/modes';
+import { getAllMaps } from '../service/maps';
+import { getAllRanks } from '../service/ranks';
 
 const HomePage = ({ agents, gameModes, maps, ranks }) => {
   const router = useRouter();
 
   const navigateReadMore = () => {
     window.location.href =
-      "https://playvalorant.com/pt-br/?gclid=CjwKCAjw9NeXBhAMEiwAbaY4lqAoe5VBlGEz10ZkOWgoPCNP9gc-OFfMN5Cf4CGBDNa0Tfd6dTh6ihoCv9kQAvD_BwE&gclsrc=aw.ds";
+      'https://playvalorant.com/pt-br/?gclid=CjwKCAjw9NeXBhAMEiwAbaY4lqAoe5VBlGEz10ZkOWgoPCNP9gc-OFfMN5Cf4CGBDNa0Tfd6dTh6ihoCv9kQAvD_BwE&gclsrc=aw.ds';
   };
 
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Hero
         image="/hero-image.png"
         text=" Um FPS tático 5x5 com personagens marcantes, mecânica de tiro precisa e
